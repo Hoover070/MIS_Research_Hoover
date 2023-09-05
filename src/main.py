c7cd12f07a2c65f18560b5b394ef883b0cdf917f
Hoover070 <Willhooverfilm@gmail.com>
2023-09-04 22:16:22 -0400

    Added goals and updated the df to include just the features I want to use

--- src/main.py
+++ src/main.py
@@ -16,11 +16,31 @@ Choose a the right model for the data
 produce visualizations
 do validation
 
+
+Goal of the project is to predict if a passenger survived the titanic disaster
+input: Pclass(Ticket Class: 1 = upper, 2 = middle, 3 = lower), Age, Parch(num parents), Sibsp(num sibling), Sex 
+output: survived
 """
 
-def load_data():
+def load_data(filename):
     """Load data from CSV files."""
     data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-    data = pd.read_csv(os.path.join(data_dir, 'titanic_passengers.csv'))
-    return data
+    path = os.path.join(data_dir, filename)
+    data = pd.read_csv(path, usecols=['Pclass', 'Age', 'Parch', 'SibSp', 'Sex', 'Survived'])
+    df = pd.DataFrame(data)
+    return df
+
+def inspect_data(data):
+    """Inspect the data."""
+    print(data.describe())
+    print(data.isnull().sum())
+
+
+if __name__ == '__main__':
+    filename = 'titanic_passengers.csv'
+    df = load_data(filename)
+    inspect_data(df)
+
+
+
 
