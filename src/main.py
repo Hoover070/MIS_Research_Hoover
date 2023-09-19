af49b1b0c7342ad84a1572ccddc5d2870f682bec
Will Hoover <billhoover070@gmail.com>
2023-09-19 19:29:18 -0400

    updated the model, using a xgboost model to find the lowest error through iteration. I am also using a seperate xgboost model to calculate the best step to use in the original xgboost model to get the lowest error.

--- src/main.py
+++ src/main.py
@@ -3,21 +3,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.decomposition import PCA
-from sklearn.dummy import DummyClassifier, DummyRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.dummy import DummyClassifier
+from sklearn.model_selection import train_test_split,  GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import KNNImputer
+import xgboost as xgb
+import warnings
+
+#added to get rid of the old python warning for seaborn
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 """
 This is a research project for Machine Intelligence Systems as Full Sail University
 
 goals are to Load the dataset 
 clean any samples or features that need it
-perform EDA
+perform EDA - determin if the data is linear or non-linear, and if it needs to be scaled
 Choose a the right model for the data 
 produce visualizations
 do validation
+
+We will then use a classification model to predict if a passenger survived the titanic
+
 
 
 Goal of the project is to predict if a passenger survived the titanic disaster
@@ -40,15 +47,9 @@ def inspect_data(data):
     print(data.info())
 
 def convert_sex(df):
-    """change Sex to int64 using oneHot"""
-  #converting object to float for Sex
-    oneshot = LabelBinarizer()
-    res = oneshot.fit_transform(df['Sex'])
-    pca = PCA(n_components=1)
-    df['Sex_PCA'] = pca.fit_transform(res)
-    df.drop(columns=['Sex'], axis=1, inplace=True)
-    df['Sex'] = pca.fit_transform(res)
-    df.drop(columns=['Sex_PCA'], axis=1, inplace=True)
+    """change Sex to binary values"""
+    df = df[df['Sex'] != '']
+    df['Sex'] = df['Sex'].replace({'male': 0, 'female': 1})
     return df
 
 def fill_age_knn(df):
@@ -68,13 +69,31 @@ def fill_age_knn(df):
 
     return df_imputed
 
+def scale_data(df):
+    """Scale the data."""
+    # create a copy of the df
+    df_scaled = df.copy()
+
+    scaler = StandardScaler()
+    scaled_values = scaler.fit_transform(df_scaled)
+
+    return pd.DataFrame(scaled_values, columns=df.columns)
+
 def correlation_plots(df, out_dir):
     """Create correlation plots."""
     # create a correlation matrix
-    correlation_matrix = df.corr(numeric_only=True)
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0)
+    correlation_matrix = df.corr()
+    plt.figure(figsize=(15, 12))
+    ax = sns.heatmap(correlation_matrix, cmap='coolwarm', center=0)
     plt.title('Correlation Matrix')
+
+
+    # Annotate the heatmap manually
+    for i in range(correlation_matrix.shape[0]):
+        for j in range(correlation_matrix.shape[1]):
+            ax.text(j + 0.5, i + 0.5, f'{correlation_matrix.iloc[i, j]:.2f}',
+                    ha='center', va='center', color='black')
+
     plt.savefig(os.path.join(out_dir, 'correlation_matrix_input_features.png'))
 
     # pairplot of the data
@@ -85,13 +104,53 @@ def correlation_plots(df, out_dir):
     plt.close(fig='all')
 
 
-def train_dummy_regressor(X_train, y_train, X_test, y_test):
-    """Train a Dummy Regressor and evaluate its score."""
-    model = DummyRegressor(strategy='mean')
+def train_dummy_classifier(X_train, y_train, X_test, y_test):
+    """Train a Dummy Classifier and evaluate its score."""
+    model = DummyClassifier()
     model.fit(X_train, y_train)
-
     score = model.score(X_test, y_test)
     return model, score
+
+
+def optimize_learning_rate(X_train, y_train):
+    """Optimize the learning rate for the XGBoost Classifier."""
+    param_grid = {
+        'learning_rate': [0.001, 0.01, 0.05, 0.1, 0.2, 0.3, .5, .75, 1, 1.25, 1.5, 2, 3, 4, 5, 10, 20, 50, 100],
+
+    }
+
+    model = xgb.XGBClassifier(objective='binary:logistic', tree_method='hist')
+
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy')
+    grid_search.fit(X_train, y_train)
+
+
+    best_score = grid_search.best_score_
+
+    return  best_score
+def xgboost_classifier(X_train, y_train, X_test, y_test, rounds=100, early_stopping_rounds=10, step_size=0.1):
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    dvalid = xgb.DMatrix(X_test, label=y_test)
+
+    params = {
+        'objective': 'binary:logistic', # binary classification (0 for dead, 1 for survived)
+        'tree_method': 'hist',
+        'eta': step_size,  # learning rate
+        'max_depth': 4,  # depth of trees (do not go too high - risk of overfitting)
+        'subsample': 0.8,  # Fraction of data to train on (helps prevent overfitting)
+        'colsample_bytree': 0.8,  # Fraction of features to train on
+        'eval_metric': 'error',
+    }
+
+    evals_result = {}
+    bst = xgb.train(params, dtrain, num_boost_round=rounds, evals=[(dvalid, 'valid')],
+                    early_stopping_rounds=early_stopping_rounds, evals_result=evals_result)
+
+    best_round = bst.best_iteration
+    best_score = evals_result['valid']['error'][best_round]
+
+    print(f"Best Round: {best_round}. Best Score: {best_score}")
+    return bst
 
 """
 input: Pclass(Ticket Class: 1 = upper, 2 = middle, 3 = lower), Age, Parch(num parents), Sibsp(num sibling), Sex 
@@ -99,15 +158,41 @@ output: survived
 """
 if __name__ == '__main__':
     filename = 'titanic_passengers.csv'
-    out_dir = './output'
+    out_dir = '../output'
+
+    """Parameters"""
+    num_rounds = 1000
+    early_stopping_rounds = 10 #number of iterations WITHOUT improvement before the model stops
     df = load_data(filename)
     df = convert_sex(df)
     df = fill_age_knn(df)
-    correlation_plots(df, out_dir)
+    #correlation_plots(df, out_dir)
+    #df = scale_data(df)
 
-    # split the data
-    input_features = df['Pclass', 'Age', 'Parch', 'SibSp', 'Sex']
-    target = df['Survived']
+    # split the data for the 1st model - XGB Classifier to predict survival of a passenger
+    input_features = df[['Pclass', 'Age', 'Parch', 'SibSp', 'Sex']]
+    who_survived = df['Survived'].astype(int)
+    X_train, X_test, y_train, y_test = train_test_split(input_features, who_survived, test_size=0.2, random_state=42)
+
+    print('Training a Dummy Classifier')
+    model, score = train_dummy_classifier(X_train, y_train, X_test, y_test)
+    print(f'Dummy Score (if you cant beat this go back to the drawing board: {score}')
+
+    print('Training a XGBoost Classifier')
+    step_size = optimize_learning_rate(X_train, y_train)
+    bst = xgboost_classifier(X_train, y_train, X_test, y_test, num_rounds, early_stopping_rounds, step_size)
+    print('Done Training')
+
+    # save the model
+    bst.save_model(os.path.join(out_dir, 'xgb_classifier.model'))
+
+    # see which features were most important in the decision
+    # results are surprising, Sex was least important with Age being most important.
+    xgb.plot_importance(bst)
+    plt.savefig(os.path.join(out_dir, 'feature_importance.png'))
+    plt.close(fig='all')
+
+
 
 
 
